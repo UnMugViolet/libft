2d6a122e3b4178a3ba8e436b42bcb6179f153583
unmugviolet <paul@rocketegg.systems>
2025-02-04 19:07:47 +0100

    update libft

--- includes/str_utils.h
+++ includes/str_utils.h
@@ -6,7 +6,7 @@
 /*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 13:50:28 by unmugviolet       #+#    #+#             */
-/*   Updated: 2025/01/31 11:13:55 by unmugviolet      ###   ########.fr       */
+/*   Updated: 2025/02/04 19:06:37 by unmugviolet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *str, const char *to_find, size_t len);
 char	*ft_strrchr(const char *str, int c);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr_free(char *str, unsigned int start,
+			size_t len);
 char	*ft_substr(char const *str, unsigned int start, size_t len);
 
 #endif
