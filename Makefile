09489b65a62acb9a7aed5e618013452972adc06f
pjaguin <pjaguin@student.42.fr>
2025-03-07 18:14:56 +0100

    Add some handy functions for printing

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/11/08 13:05:36 by pjaguin           #+#    #+#              #
-#    Updated: 2025/02/14 18:46:00 by pjaguin          ###   ########.fr        #
+#    Updated: 2025/03/07 15:23:37 by pjaguin          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -42,7 +42,7 @@ CONVERSIONS_FILES = ft_atoi.c ft_itoa.c ft_nbr_len.c ft_str_tolower.c ft_str_tou
 
 MEMORY_FILES 	  = ft_bzero.c ft_calloc.c ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memmove.c ft_memset.c
 
-PRINT_FILES		  = ft_count_putchar_fd.c ft_count_putstr_fd.c ft_putchar_fd.c ft_putendl_fd.c ft_putnbr_fd.c ft_putstr_fd.c
+PRINT_FILES		  = ft_count_putchar_fd.c ft_count_putstr_fd.c ft_putchar_fd.c ft_putendl_fd.c ft_putnbr_fd.c ft_putstr_fd.c ft_print_array_str.c \
 
 SORT_FILES		  = ft_reverse.c
 
--- srcs/print/ft_print_array_str.c
+++ srcs/print/ft_print_array_str.c
@@ -1,27 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print.h                                            :+:      :+:    :+:   */
+/*   ft_print_array_str.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/29 13:45:30 by unmugviolet       #+#    #+#             */
-/*   Updated: 2025/03/07 15:23:13 by pjaguin          ###   ########.fr       */
+/*   Created: 2025/03/07 15:20:12 by pjaguin           #+#    #+#             */
+/*   Updated: 2025/03/07 15:27:04 by pjaguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINT_H
-# define PRINT_H
+#include "print.h"
 
-# include "libft.h"
+void	ft_print_array_str(char **array)
+{
+	int	i;
 
-size_t	ft_count_putchar_fd(char c, int fd);
-size_t	ft_count_putstr_fd(char *str, int fd);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putendl_fd(char *str, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putstr_fd(char *str, int fd);
-void	ft_print_array_str(char **array);
-void	ft_print_array_str(char **array);
-
-#endif
+	i = -1;
+	if (!array || !array[0])
+		return ;
+	while (array[++i])
+		ft_putendl_fd(array[i], 1);
+}
