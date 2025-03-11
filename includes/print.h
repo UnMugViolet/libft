72e130633ea81a4544fddd9251b6f3b27a6a08e4
UnUmgViolet <jaguinpaul@gmail.com>
2025-03-11 18:15:44 +0100

    Fix merge conflicts

--- includes/print.h
+++ includes/print.h
@@ -3,17 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-<<<<<<< HEAD
-/*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 13:45:30 by unmugviolet       #+#    #+#             */
-/*   Updated: 2025/03/07 15:23:13 by pjaguin          ###   ########.fr       */
-=======
-/*   By: fureimu <fureimu@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/29 13:45:30 by unmugviolet       #+#    #+#             */
-/*   Updated: 2025/03/11 14:29:36 by fureimu          ###   ########.fr       */
->>>>>>> a790b96
+/*   Updated: 2025/03/11 18:14:01 by unmugviolet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +21,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *str, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *str, int fd);
-<<<<<<< HEAD
-void	ft_print_array_str(char **array);
-void	ft_print_array_str(char **array);
-=======
 void	ft_print_array_str_fd(char **array, int fd);
->>>>>>> a790b96
 
 #endif
