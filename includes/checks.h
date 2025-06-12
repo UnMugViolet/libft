87b41c095ca2814abbbe01da37fb79359bdd2cc7
pjaguin <pjaguin@student.42.fr>
2025-06-12 11:30:19 +0200

    Updated the libft to include ft_is_charset function

--- includes/checks.h
+++ includes/checks.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   checks.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
+/*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 13:37:28 by unmugviolet       #+#    #+#             */
-/*   Updated: 2025/01/29 13:57:17 by unmugviolet      ###   ########.fr       */
+/*   Updated: 2025/06/12 11:28:15 by pjaguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 # define CHECKS_H
 
 # include "libft.h"
+# include <stdbool.h>
 
-int	ft_is_whitespace(int c);
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
+int		ft_is_whitespace(int c);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+bool	ft_is_charset(char c, char *charset);
 
 #endif
--- srcs/checks/ft_is_charset.c
+++ srcs/checks/ft_is_charset.c
@@ -3,22 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_is_charset.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fureimu <fureimu@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 13:04:04 by fureimu           #+#    #+#             */
-/*   Updated: 2025/03/11 13:05:53 by fureimu          ###   ########.fr       */
+/*   Updated: 2025/06/12 11:27:51 by pjaguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checks.h"
 
-int	ft_is_charset(char c, char *charset)
+/*
+*	Checks wether a given char is part of the charset.
+*	If no charset provided it returns false
+*	@return(bool)
+*/
+bool	ft_is_charset(char c, char *charset)
 {
 	int	i;
 
 	i = -1;
+	if (!charset)
+		return (false);
 	while (charset[++i])
 		if (charset[i] == c)
-			return (1);
-	return (0);
+			return (true);
+	return (false);
 }
